6b29ee516b0c8946843fa558860f0b4802a7a321
inje98 <kij980723@gmail.com>
2024-01-29 16:52:00 +0900

    1일차 내용 정리

--- day01/test04_operator.py
+++ day01/test04_operator.py
@@ -14,7 +14,13 @@ print(5 // 2) # 2  정수로 나누기
 print(5 % 2) # 정수로 나누고 남은 나머지
 
 print(5 == 4) # False
-print(5 > 4) # True
+print(5 > 4)  # True
 print(5 >= 4) # True
 print(5 <= 4) # False
 print(5 != 4) # True
+
+print(5 <= 4 and (5/2 < 3)) # False 
+print(5 <= 4 or (5/2 < 3)) # True
+print(not (5 < 4)) # True #참이면 거짓으로, 거짓이면 참으로
+
+
